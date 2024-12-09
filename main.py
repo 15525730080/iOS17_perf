d01a360b873b8b23bf333f5503335750b7c6d9f7
范博洲 <153100629+15525730080@users.noreply.github.com>
2024-12-09 13:04:55 +0800

    脚本默认使用sudo/管理员权限启动；自动安装依赖库；

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-            
 # @Author: 范博洲
+import ctypes
 import dataclasses
 import os
 import platform
@@ -10,13 +11,21 @@ import threading
 import time
 from datetime import datetime
 
+try:
+    import ios_device
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "py_ios_device"])
+try:
+    import pymobiledevice3
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pymobiledevice3"])
 from ios_device.cli.base import InstrumentsBase
 from ios_device.cli.cli import print_json
 from ios_device.remote.remote_lockdown import RemoteLockdownClient
 from ios_device.util.utils import convertBytes
 
 
-class TunnelManager:
+class TunnelManager(object):
     def __init__(self):
         self.start_event = threading.Event()
         self.tunnel_host = None
@@ -28,26 +37,25 @@ class TunnelManager:
                                   stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
             while not rp.poll():
-                try:
-                    line = rp.stdout.readline().decode()
-                except:
-                    print("decode fail {0}".format(line))
-                    continue
+                line = rp.stdout.readline().decode()
                 line = line.strip()
                 if line:
                     print(line)
+                assert "ERROR Device is not connected" not in line, "ERROR Device is not connected"
                 if "--rsd" in line:
                     ipv6_pattern = r'--rsd\s+(\S+)\s+'
                     port_pattern = r'\s+(\d{1,5})\b'
                     self.tunnel_host = re.search(ipv6_pattern, line).group(1)
+                    print(self.tunnel_host)
                     self.tunnel_port = int(re.search(port_pattern, line).group(1))
+                    print(port_pattern)
                     self.start_event.set()
 
         threading.Thread(target=start_tunnel).start()
-        self.start_event.wait(timeout=15)
+        self.start_event.wait(timeout=30)
 
 
-class PerformanceAnalyzer:
+class PerformanceAnalyzer(object):
     def __init__(self, udid, host, port):
         self.udid = udid
         self.host = host
@@ -98,19 +106,41 @@ class PerformanceAnalyzer:
                 rpc.graphics(on_callback_fps_message, 1000)
 
 
-if __name__ == '__main__':
-    if "Windows" in platform.platform():
-        import ctypes
-        assert ctypes.windll.shell32.IsUserAnAdmin() == 1, "必须使用管理员权限启动"
+def check_admin():
+    if platform.system() == "Windows":
+        return os.getuid() == 0  # Windows管理员权限检查
+    else:  # Linux or macOS
+        return os.geteuid() == 0  # Linux/Mac管理员权限检查
+
+
+def run_with_admin_privileges(command):
+    if platform.system() == "Windows":
+        # Windows上以管理员权限运行
+        ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, " ".join(sys.argv), None, 1
+        )
+        sys.exit(0)  # 退出当前进程
     else:
-        assert os.geteuid() == 0, "必须使用sudo权限启动"
-    bundle_id = "com.alipay.iphoneclient"
-    udid = "00008110-0012148E1E8B801E"
+        # Linux上使用sudo运行
+        subprocess.run(['sudo', sys.executable] + command, check=True)
+
+
+# 检查是否具有管理员权限
+if not check_admin():
+    print("没有管理员权限，尝试以管理员权限运行...")
+    # 将当前脚本的路径和参数传递给run_with_admin_privileges函数
+    run_with_admin_privileges(sys.argv)
+    sys.exit()  # 退出当前进程
+
+if __name__ == '__main__':
+    udid = ""  # 目标设备
+    bundle_id = ""  # 目标app
+    if len(sys.argv) >= 3:
+        udid = sys.argv[1]
+        bundle_id = sys.argv[2]
     tunnel_manager = TunnelManager()
     tunnel_manager.get_tunnel()
-
     performance_analyzer = PerformanceAnalyzer(udid, tunnel_manager.tunnel_host, tunnel_manager.tunnel_port)
-
     threading.Thread(target=performance_analyzer.ios17_proc_perf, args=(bundle_id,)).start()
     time.sleep(0.1)
     threading.Thread(target=performance_analyzer.ios17_fps_perf).start()
